8dca4afdf169274be494f3102cf8f1cac255ddbf
abderrahim elouardy <elouardyabderrahim07@gmail.com>
2022-12-05 17:01:48 +0100

    first commit

---
+++ galloryBack/src/main/java/com/mygallery/MygalleryApplication.java
@@ -0,0 +1,13 @@
+package com.mygallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygalleryApplication.class, args);
+	}
+
+}
