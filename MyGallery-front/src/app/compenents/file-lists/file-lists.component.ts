aec67f6951f1c9a142fdc958b75fad7c4d0ebcf5
omardbaa <dbaaomar18@gmail.com>
2022-12-12 17:43:19 +0100

    Extension binding and update UI

--- MyGallery-front/src/app/compenents/file-lists/file-lists.component.ts
+++ MyGallery-front/src/app/compenents/file-lists/file-lists.component.ts
@@ -1,3 +1,4 @@
+import { HttpEventType, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -37,7 +38,13 @@ export class FileListsComponent implements OnInit {
 
 
 
-  
+  file: FileModule = new FileModule();
+ 
+  selectedFiles?: FileList;
+  currentFile?: File;
+  progress = 0;
+  message = '';
+
 
 
 
@@ -64,6 +71,65 @@ export class FileListsComponent implements OnInit {
       this.getFiles();
     })
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+ 
+
+
+
+    selectFile(event: any): void {
+      this.selectedFiles = event.target.files;
+    }
+  
+    upload(): void {
+      this.progress = 0;
+  
+      if (this.selectedFiles) {
+        const file: File | null = this.selectedFiles.item(0);
+  
+        if (file) {
+          this.currentFile = file;
+  
+          this.fileSrvice.upload(this.currentFile).subscribe({
+            next: (event: any) => {
+              if (event.type === HttpEventType.UploadProgress) {
+                this.progress = Math.round(100 * event.loaded / event.total);
+              } else if (event instanceof HttpResponse) {
+                this.message = event.body.message;
+                
+              }
+            },
+            error: (err: any) => {
+              console.log(err);
+              this.progress = 0;
+  
+              if (err.error && err.error.message) {
+                this.message = err.error.message;
+              } else {
+                this.message = 'Could not upload the file!';
+              }
+  
+              this.currentFile = undefined;
+            }
+          });
+        }
+  
+        this.selectedFiles = undefined;
+      }
+    }
 
 
 }
