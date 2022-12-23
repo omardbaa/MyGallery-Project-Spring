233520ca5a36579f25c075919999c6cbbd89b3ff
elouardyabderrahim <elouardy.abderrahim07@gmail.com>
2022-12-23 11:01:03 +0100

    feature:adding search with pagination and sorting in the same URL

--- galloryBack/src/main/java/com/mygallery/enities/FileResponse.java
+++ galloryBack/src/main/java/com/mygallery/enities/FileResponse.java
@@ -10,8 +10,8 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class FileResponse {
-    private List<FileDto> content;
+public class FileResponse<T> {
+    private List<T> content;
     private int pageNo;
     private int pageSize;
     private long totalElements;
