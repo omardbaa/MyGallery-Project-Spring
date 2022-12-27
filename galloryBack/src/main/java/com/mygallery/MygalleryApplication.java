5d8444dc9a3685233a4dee7a1be634630d8cd97a
omardbaa <dbaaomar18@gmail.com>
2022-12-27 09:08:14 +0100

    devops: Added home route (/)

--- galloryBack/src/main/java/com/mygallery/MygalleryApplication.java
+++ galloryBack/src/main/java/com/mygallery/MygalleryApplication.java
@@ -6,12 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@RestController
 @SpringBootApplication
 public class MygalleryApplication extends ResponseEntityExceptionHandler {
-
+	@RequestMapping("/")    String home() {        return "Hello World!";    }
 	public static void main(String[] args) {
 		SpringApplication.run(MygalleryApplication.class, args);
 	}
